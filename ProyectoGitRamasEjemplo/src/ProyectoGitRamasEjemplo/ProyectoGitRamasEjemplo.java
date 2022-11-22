10ffaf7a8ac72f11abe7142abd61274a23cef554
Jesús De Dios.R4 <Jess De Dios.R4@DESKTOP-5FAD0MS>
2022-11-22 20:58:32 +0100

    Cambio de palabra
    Cambio hello por hola

--- ProyectoGitRamasEjemplo/src/ProyectoGitRamasEjemplo/ProyectoGitRamasEjemplo.java
+++ ProyectoGitRamasEjemplo/src/ProyectoGitRamasEjemplo/ProyectoGitRamasEjemplo.java
@@ -5,7 +5,7 @@ import javax.swing.JOptionPane;
 public class ProyectoGitRamasEjemplo {
 
 	public static void main(String[] args) {
-		JOptionPane.showMessageDialog(null, "Hola mundo");
+		JOptionPane.showMessageDialog(null, "hello mundo");
 		int variablexM;
 	}
 
