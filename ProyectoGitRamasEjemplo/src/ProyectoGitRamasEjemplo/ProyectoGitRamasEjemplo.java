e71facaef98b5a431817b25b8e8aca83394c21ad
Jesús De Dios.R4 <Jess De Dios.R4@DESKTOP-5FAD0MS>
2022-11-22 20:51:47 +0100

    Commit inicial
    Class de Hola mundo por pantalla

---
+++ ProyectoGitRamasEjemplo/src/ProyectoGitRamasEjemplo/ProyectoGitRamasEjemplo.java
@@ -0,0 +1,11 @@
+package ProyectoGitRamasEjemplo;
+
+import javax.swing.JOptionPane;
+
+public class ProyectoGitRamasEjemplo {
+
+	public static void main(String[] args) {
+		JOptionPane.showMessageDialog(null, "Hola mundo");
+	}
+
+}
